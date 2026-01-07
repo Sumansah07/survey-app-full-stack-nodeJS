e13bcafa7c3a7df71d6b7dfba45e9aa93bcec619
Suman Sah <152900053+Sumansah07@users.noreply.github.com>
2026-01-07 22:38:49 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -19,4 +19,9 @@ app.use('/api/surveys', require('./routes/surveys'));
 app.use('/api/responses', require('./routes/responses'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
